edd09d1214aea7ca7862ed4be9f075b36b0b0bf6
Subhash Barela <barelasubhash16@gmail.com>
2023-02-17 00:25:31 -0800

    commit the program

---
+++ QuitiontAndRemider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoding
+{
+    internal class QuitiontAndRemider
+    {
+        public static void QuitiontReminder()
+        {
+            Console.Write("Entert the divident : ");
+            double dividend=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Entert the divident : ");
+            double divisor=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("\n");
+           
+
+            double quotient = dividend / divisor;
+            double remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0}   Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+
+        }
+    }
+}
